54a59988b54c11322a012e6d3ef774c9234609b5
Zan Jamieson <akjamies@ncsu.edu>
2026-09-18 19:53:33 -0400

    sanity stuff and interactable

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,9 +1,16 @@
 extends CharacterBody2D
-var health
+var health: int;
+var sanity: float;
+var timeSinceLastSanityTick: float;
+var sanityTick: float;
 
 func _ready():
 	health = 10
-	updateLabel();
+	sanity = 1000;
+	timeSinceLastSanityTick = 0;
+	sanityTick = 1;
+	$".."/sanity.text = str(sanity);
+	updateHealthLabel();
 
 func _process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down");
@@ -17,12 +24,23 @@ func _process(delta):
 		$Sprite2D.rotation_degrees = 180;
 	elif (direction.y == -1):
 		$Sprite2D.rotation_degrees = 0;
+	
+	timeSinceLastSanityTick += delta;
+	if (timeSinceLastSanityTick >= 1):
+		
+		sanity -= sanityTick;
+		sanityTick *= 1.1;
+		timeSinceLastSanityTick = 0;
+		$".."/sanity.text = str(sanity);
 
 func _on_hazard_body_entered(body: Node2D) -> void:
-	health -= 1;
-	updateLabel();
+	hurt();
 
-func updateLabel():
+func hurt():
+	health -= 1;
+	updateHealthLabel();
+
+func updateHealthLabel():
 	if (health >= 5):
 		$".."/RightHand.animation = "0";
 		$".."/LeftHand.animation = str(10-health);
