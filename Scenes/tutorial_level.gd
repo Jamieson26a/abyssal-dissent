1e2a5b2e2580d535f6b8ebcbca12721f7b4e11af
Hudson-White <hudsonforpresident@gmail.com>
2026-09-19 19:17:37 -0400

    Hudson Update 11

--- Scenes/tutorial_level.gd
+++ Scenes/tutorial_level.gd
@@ -8,3 +8,28 @@ func _ready() -> void:
 		"end": [Vector2(151.0, 1048.0), 1, 1],
 		"thingy": [Vector2(1000, 1100), 0, -5]
 	}
+
+
+func _on_interactable_body_entered(body: Node2D) -> void:
+	$Interactable/Label.visible = true;
+
+func _on_interactable_body_exited(body: Node2D) -> void:
+	$Interactable/Label.visible = false;
+
+func _on_interactable_2_body_entered(body: Node2D) -> void:
+	$Interactable2/Label.visible = true;
+
+func _on_interactable_2_body_exited(body: Node2D) -> void:
+	$Interactable2/Label.visible = false;
+
+func _on_interactable_3_body_entered(body: Node2D) -> void:
+	$Interactable3/Label.visible = true;
+
+func _on_interactable_3_body_exited(body: Node2D) -> void:
+	$Interactable3/Label.visible = false;
+
+func _on_interactable_4_body_entered(body: Node2D) -> void:
+	$Interactable4/Label.visible = true;
+
+func _on_interactable_4_body_exited(body: Node2D) -> void:
+	$Interactable4/Label.visible = false;
