550dda55d11d91fa61acf23481deda1338cacfa8
Hudson-White <hudsonforpresident@gmail.com>
2026-09-19 18:07:43 -0400

    Hudson Update 10

--- player.gd
+++ player.gd
@@ -7,6 +7,7 @@ var sanityInt10: int
 var sanityString: String
 
 @export var player_music_player: AudioStreamPlayer
+@export var walk_music_player: AudioStreamPlayer2D
 
 var yielding: bool;
 
@@ -57,6 +58,10 @@ func _process(delta):
 			$Sprite2D.rotation_degrees = 90;
 		elif (direction == Vector2(0, -1) or direction.is_equal_approx(Vector2(-0.707107, -0.707107))):
 			$Sprite2D.rotation_degrees = 270;
+		if direction != Vector2(0,0):
+			walk_music_player["stream_paused"] = false
+		else:
+			walk_music_player["stream_paused"] = true
 	else:
 		direction = Vector2(0,0)
 		sanityTick = 20;
