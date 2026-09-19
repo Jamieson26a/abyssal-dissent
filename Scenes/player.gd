ac9799df7e790aaddf8cb56eb5e4b6eb6e17e5c5
Shanley Brown <mbbrown3@ncsu.edu>
2026-09-18 21:34:21 -0400

    Help Im stuck

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,36 +1,56 @@
 extends CharacterBody2D
-var health
+var sanity: float;
+var timeSinceLastSanityTick: float;
+var sanityTick: float;
+var player_vars;
 
 func _ready():
-	health = 10
-	updateLabel();
+	player_vars = get_node("/root/PlayerVariables");
+	timeSinceLastSanityTick = 0;
+	sanityTick = 50;
+	updateHealthLabel();
+	sanityUpdate();
 
 func _process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down");
 	velocity = direction * 1000;
 	move_and_slide();
 	if (direction.x == 1):
-		$Sprite2D.rotation_degrees = 90;
-	elif (direction.x == -1):
-		$Sprite2D.rotation_degrees = 270;
-	elif (direction.y == 1):
-		$Sprite2D.rotation_degrees = 180;
-	elif (direction.y == -1):
 		$Sprite2D.rotation_degrees = 0;
+	elif (direction.x == -1):
+		$Sprite2D.rotation_degrees = 180;
+	elif (direction.y == 1):
+		$Sprite2D.rotation_degrees = 90;
+	elif (direction.y == -1):
+		$Sprite2D.rotation_degrees = 270;
+		
+	if (direction.length() > 0):
+		$Sprite2D.play("walking")
+	else:
+		$Sprite2D.animation = "default"
+		
+	timeSinceLastSanityTick += delta;
+	if (timeSinceLastSanityTick >= 1):
+		player_vars.sanity -= sanityTick;
+		timeSinceLastSanityTick = 0;
+		sanityUpdate();
+
+func sanityUpdate():
+	var level:int = int(player_vars.sanity/100) + 1;
+	$RightHand/Eye.animation = str(level) + player_vars.approval;
 
 func _on_hazard_body_entered(body: Node2D) -> void:
-	var player_vars = get_node("/root/PlayerVariables")
+	hurt();
+
+func hurt():
 	player_vars.health -= 1
-	updateLabel();
-	
+	updateHealthLabel();
 
-
-func updateLabel():
-	var player_vars = get_node("/root/PlayerVariables")
-	if (health >= 5):
+func updateHealthLabel():
+	if (player_vars.health >= 5):
 		$RightHand.animation = "0";
 		$LeftHand.animation = str(10-player_vars.health);
-		print("ow")
-	elif (health >= 0):
+	elif (player_vars.health >= 0):
 		$RightHand.animation = str(5-player_vars.health);
 		$LeftHand.animation = "5";
+	
