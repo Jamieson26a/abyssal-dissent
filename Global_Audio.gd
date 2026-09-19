f640282e2886dc01caea1a7857e55717f62fe4f9
Hudson-White <hudsonforpresident@gmail.com>
2026-09-19 12:38:12 -0400

    Hudson Update 5

---
+++ Global_Audio.gd
@@ -0,0 +1,5 @@
+extends Node
+
+var current_sanity: String
+var music_volume: int
+var sfx_volume: int
--- player.gd
+++ player.gd
@@ -3,11 +3,16 @@ var sanity: float;
 var timeSinceLastSanityTick: float;
 var sanityTick: float;
 var player_vars;
+var sanityInt10: int
+var sanityString: String
+
+@export var player_music_player: AudioStreamPlayer
 
 func _ready():
 	player_vars = get_node("/root/PlayerVariables");
 	timeSinceLastSanityTick = 0;
-	sanityTick = 50;
+	sanityTick = 20;
+	sanityInt10 = int(player_vars.sanity/100)
 	updateHealthLabel();
 	sanityUpdate();
 
@@ -34,6 +39,26 @@ func _process(delta):
 		player_vars.sanity -= sanityTick;
 		timeSinceLastSanityTick = 0;
 		sanityUpdate();
+	
+	sanityInt10 = int(player_vars.sanity/100)
+	
+	if sanityInt10 > 0 and 3 >= sanityInt10:
+		sanityString = "low"
+	
+	if sanityInt10 > 3 and 6 >= sanityInt10:
+		sanityString = "mid"
+	
+	if sanityInt10 > 6 and 10 >= sanityInt10:
+		sanityString = "high"
+	
+	if sanityString != GlobalAudio.current_sanity:
+		GlobalAudio.current_sanity = sanityString
+		update_music_for_sanity()
+		
+
+func update_music_for_sanity():
+	var current_sanity_music = str(sanityString + "sanity")
+	player_music_player["parameters/switch_to_clip"] = current_sanity_music
 
 func sanityUpdate():
 	var level:int = int(player_vars.sanity/100) + 1;
@@ -44,7 +69,7 @@ func _on_hazard_body_entered(body: Node2D) -> void:
 
 func _on_tutorial_pit_body_entered(body: Node2D) -> void:
 	get_tree().change_scene_to_file("res://mainmenu.tscn")
-	resetafterexit()
+	resetafterexitorenter()
 	print("bye")
 
 func hurt():
@@ -59,7 +84,7 @@ func updateHealthLabel():
 		$RightHand.animation = str(5-player_vars.health);
 		$LeftHand.animation = "5";
 	
-func resetafterexit():
+func resetafterexitorenter():
 	player_vars.health = 10
 	player_vars.sanity = 1000
 	
@@ -67,3 +92,9 @@ func resetafterexit():
 func _on_next_room_pit_1_body_entered(body: Node2D) -> void:
 	get_tree().change_scene_to_file("res://Scenes/Rooms/level_2.tscn")
 	print("boo")
+
+
+func _on_buggy_hit_box_body_entered(body: Node2D) -> void:
+	hurt()
+	print("buggy")
+	
