5d1d0fb76cf540c064ad8612488141514ff34830
Zan Jamieson <akjamies@ncsu.edu>
2026-09-19 14:51:34 -0400

    BUGGY MOVES

--- Assets/buggy.gd
+++ Assets/buggy.gd
@@ -1,25 +1,39 @@
 extends CharacterBody2D
 
+var movement_speed: float = 200.0
+var movement_target_position: Vector2;
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
 
+func _ready():
+	# These values need to be adjusted for the actor's speed
+	# and the navigation layout.
+	navigation_agent.path_desired_distance = 4.0
+	navigation_agent.target_desired_distance = 4.0
+	movement_target_position = $".."/Player.position;
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	# Make sure to not await during _ready.
+	actor_setup.call_deferred()
+	
+func _process(delta):
+	set_movement_target($".."/Player.position);
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+func actor_setup():
+	# Wait for the first physics frame so the NavigationServer can sync.
+	await get_tree().physics_frame
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	# Now that the navigation map is no longer empty, set the movement target.
+	set_movement_target(movement_target_position)
 
+func set_movement_target(movement_target: Vector2):
+	navigation_agent.target_position = movement_target
+
+func _physics_process(delta):
+	if navigation_agent.is_navigation_finished():
+		return
+
+	var current_agent_position: Vector2 = global_position
+	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
+
+	velocity = current_agent_position.direction_to(next_path_position) * movement_speed
 	move_and_slide()
